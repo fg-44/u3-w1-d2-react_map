a6e710f2cf2a10a5e9f98929d91d0be405125f39
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2023-12-19 16:30:11 +0100

    UPDATE INIZIALE

---
+++ u3-w1-d2/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BasicExample from './Component/header';
+import './App.css';
+
+function App() {
+  return (
+       <BasicExample></BasicExample> 
+
+  );
+}
+
+export default App;
